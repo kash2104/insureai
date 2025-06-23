2c228b83f090d72394e976efc7c49335a32d3ff7
kash2104 <kparikh1104@gmail.com>
2025-06-24 04:21:52 +0530

    feat: Added auth middleware.

---
+++ Server/middlewares/auth.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+
+exports.auth = (req, res, next) => {
+    try {
+        const token = req.cookies.token || req.header('Authorization')?.replace('Bearer ', '');
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: 'Unauthorized access. No token provided.',
+            });
+        }
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decoded; 
+        } 
+        catch (error) {
+            console.error('Authentication error:', error);
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid token. Please log in again.',
+            });
+        }
+                
+        next();
+    } catch (error) {
+       return res.status(401).json({
+            success: false,
+            message: 'Unauthorized access. Please log in.',
+            error: error.message,
+       }) 
+    }
+}
