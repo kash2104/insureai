1d24e07390b4f4b603b6ea03da4f0fd03f6079c8
kash2104 <kparikh1104@gmail.com>
2025-06-24 11:09:04 +0530

    feat: Setup insurance routing.

---
+++ Server/routes/extractInsurance.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const { extractInsurance, getInsuranceDetails, findSimilarInsurance } = require('../controllers/Insurance');
+const { auth } = require('../middlewares/auth');
+
+router.post('/extract', auth, extractInsurance);
+
+router.post('/insurance', auth, getInsuranceDetails);
+
+router.post('/similarInsurance', auth, findSimilarInsurance);
+
+module.exports = router;
