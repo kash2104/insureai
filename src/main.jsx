e6faf6ea1427262c238408903973ecf6cb83b922
kash2104 <kparikh1104@gmail.com>
2025-06-24 22:38:35 +0530

    feat: Added google auth.

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import InsuranceUploadPage from './pages/InsuranceUpload.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/upload' element={<InsuranceUploadPage />} />
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  </StrictMode>,
+)
