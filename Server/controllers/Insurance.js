2bcbefd1066e77a4e46023e5470a428374027e05
kash2104 <kparikh1104@gmail.com>
2025-06-24 11:08:37 +0530

    feat: Added different insurance functions.

---
+++ Server/controllers/Insurance.js
@@ -0,0 +1,106 @@
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const { extractInsuranceFields } = require('../utils/llmproxy');
+const { sendDataToSummaryQueue } = require('../config/queue');
+const { webSearch } = require('../utils/websearch');
+
+exports.extractInsurance = async (req, res) => {
+  try {
+    // Check if file is present
+    const insurance = req.files?.file;
+    if (!insurance) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    // Check if accessToken is present
+    if (!req.user || !req.user.accessToken) {
+      return res.status(401).json({ error: 'No access token provided' });
+    }
+
+    // Prepare form-data
+    const form = new FormData();
+    form.append('file', fs.createReadStream(insurance.tempFilePath));
+
+    // Send request to Alchemist API
+    const response = await axios.post('https://platform-backend.getalchemystai.com/api/v1/upload', form, {
+      headers: {
+        ...form.getHeaders(),
+        Authorization: `Bearer ${req.user.accessToken}`,
+      },
+    });
+
+    if(response){
+      await sendDataToSummaryQueue({
+        id: req.user.id,
+        text: response.data.text,
+        accessToken: req.user.accessToken,
+      })
+    }
+    // Forward the response from Alchemist API
+    return res.status(200).json({
+        success: true,
+        data: response.data,
+    });
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+    res.status(500).json({ error: err.message });
+  }
+}
+
+exports.getInsuranceDetails = async(req, res) => {
+    try{
+        if(!req.user.accessToken){
+            return res.status(401).json({ error: 'No access token provided' });
+        }
+
+        const {insuranceData} = req.body;
+
+        const response = await extractInsuranceFields(insuranceData);
+
+        return res.status(200).json({
+            success: true,
+            message: "Got the insurance fields",
+            data: response,
+        })
+    }
+    catch(error){
+        console.error(error);
+        return res.status(500).json({
+            success:false,
+            error: error,
+            message: 'Could not get insurance fields',
+        })
+    }
+}
+
+exports.findSimilarInsurance = async(req, res) => {
+  try {
+    const {summary} = req.body;
+    if (!summary) {
+      return res.status(400).json({ error: 'Summary is required' });
+    }
+    if (!req.user || !req.user.accessToken) {
+      return res.status(401).json({ error: 'No access token provided' });
+    }
+
+    const webSearchResults = await webSearch(summary, req.user.accessToken);
+    if (!webSearchResults) {
+      return res.status(404).json({ error: 'No similar insurance plans found' });
+    }
+
+
+    return res.status(200).json({
+      success: true,
+      data: webSearchResults,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+      message: 'Could not find similar insurance plans',
+    });
+    
+  }
+}
