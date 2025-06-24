b5fa2d7c2cea574d97d7e184a51c6ec5c1e99c39
kash2104 <kparikh1104@gmail.com>
2025-06-24 23:47:19 +0530

    refactor: Adjusted the background height.

--- src/components/ui/aurorabackground.jsx
+++ src/components/ui/aurorabackground.jsx
@@ -24,7 +24,7 @@ export const AuroraBackground = ({
   return (
     <div
       className={cn(
-        "relative flex flex-col h-[100vh] items-center justify-center bg-zinc-50 dark:bg-zinc-900 text-slate-950 transition-bg",
+        "relative flex flex-col min-h-screen items-center justify-center bg-zinc-50 dark:bg-zinc-900 text-slate-950 transition-bg",
         className
       )}
       onMouseMove={handleMouseMove}
