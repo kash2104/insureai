681e96df357c9916f251905cc93acb4c1d397542
kash2104 <kparikh1104@gmail.com>
2025-06-30 14:37:18 +0530

    refactor: Remove logs.

--- Server/config/queue.js
+++ Server/config/queue.js
@@ -1,121 +1,125 @@
-const amqplib = require('amqplib');
-const { extractInsuranceFields } = require('../utils/llmproxy');
-const { webSearch } = require('../utils/websearch');
-const { publisher, CHANNEL } = require('./pubsub');
+const amqplib = require("amqplib");
+const { extractInsuranceFields } = require("../utils/llmproxy");
+const { webSearch } = require("../utils/websearch");
+const { publisher, CHANNEL } = require("./pubsub");
 
 let connection, channel;
-async function connectQueue(){
-    try {
-        const rabbitmqurl = process.env.RABBITMQ_URL || "amqp://localhost";
-        connection = await amqplib.connect(rabbitmqurl);
+async function connectQueue() {
+  try {
+    const rabbitmqurl = process.env.RABBITMQ_URL || "amqp://localhost";
+    connection = await amqplib.connect(rabbitmqurl);
 
-        channel = await connection.createChannel();
+    channel = await connection.createChannel();
 
-        await channel.assertQueue('summariseQueue');
+    await channel.assertQueue("summariseQueue");
 
-        await channel.assertQueue('websearchQueue');
+    await channel.assertQueue("websearchQueue");
 
-        console.log('Connected to RabbitMQ and queue is ready');
-    }
-    catch(error){
-        console.error('Error connecting to RabbitMQ:', error);
-        throw new Error('Failed to connect to RabbitMQ');
-        
-    }
+    console.log("Connected to RabbitMQ and queue is ready");
+  } catch (error) {
+    console.error("Error connecting to RabbitMQ:", error);
+    throw new Error("Failed to connect to RabbitMQ");
+  }
 }
 
-
-async function sendDataToSummaryQueue(data){
-    try{
-        console.log(Buffer.from(JSON.stringify(data)));
-        await channel.sendToQueue('summariseQueue', Buffer.from(JSON.stringify(data)));
-        console.log('Data sent to summary worker');
-    }
-    catch(error){
-        console.error('Error sending data to summariseQueue:', error);
-        throw new Error('Failed to send data to summariseQueue');
-    }
+async function sendDataToSummaryQueue(data) {
+  try {
+    // console.log(Buffer.from(JSON.stringify(data)));
+    await channel.sendToQueue(
+      "summariseQueue",
+      Buffer.from(JSON.stringify(data))
+    );
+    console.log("Data sent to summary worker");
+  } catch (error) {
+    console.error("Error sending data to summariseQueue:", error);
+    throw new Error("Failed to send data to summariseQueue");
+  }
 }
 
-async function sendDataToWebSearchQueue(data){
-    try{
-        console.log(Buffer.from(JSON.stringify(data)));
-        await channel.sendToQueue('websearchQueue', Buffer.from(JSON.stringify(data)));
-        console.log('Data sent to web search worker');
-    }
-    catch(error){
-        console.error('Error sending data to websearchQueue:', error);
-        throw new Error('Failed to send data to websearchQueue');
-    }
+async function sendDataToWebSearchQueue(data) {
+  try {
+    // console.log(Buffer.from(JSON.stringify(data)));
+    await channel.sendToQueue(
+      "websearchQueue",
+      Buffer.from(JSON.stringify(data))
+    );
+    console.log("Data sent to web search worker");
+  } catch (error) {
+    console.error("Error sending data to websearchQueue:", error);
+    throw new Error("Failed to send data to websearchQueue");
+  }
 }
 
-async function summaryWorker(){
-    try {
-        console.log('summary worker started');
-        channel.consume('summariseQueue', async (data) => {
-            if (data !== null) {
-                const {id, text, accessToken} = JSON.parse(data.content.toString());
-                console.log('Received data for summarization:', text);
-                
-                // Process the data here
-                // For example, you can call a function to summarize the data
-                const summary = await extractInsuranceFields(text);
+async function summaryWorker() {
+  try {
+    console.log("summary worker started");
+    channel.consume("summariseQueue", async (data) => {
+      if (data !== null) {
+        const { id, text, accessToken } = JSON.parse(data.content.toString());
+        // console.log("Received data for summarization:", text);
 
-                await sendDataToWebSearchQueue({
-                    id,
-                    summary,
-                    accessToken
-                })
-                
-                // Acknowledge the message
-                channel.ack(data);
-            }
+        // Process the data here
+        // For example, you can call a function to summarize the data
+        const summary = await extractInsuranceFields(text);
+
+        await sendDataToWebSearchQueue({
+          id,
+          summary,
+          accessToken,
         });
-    } catch (error) {
-        console.error('Error in summary worker:', error);
+
+        // Acknowledge the message
         channel.ack(data);
-    }
+      }
+    });
+  } catch (error) {
+    console.error("Error in summary worker:", error);
+    channel.ack(data);
+  }
 }
 
-async function websearchWorker(){
-    try {
-        console.log(`websearch worker started`);
-        await channel.consume('websearchQueue', async (data) => {
-            if (data !== null) {
-                const {id, summary, accessToken} = JSON.parse(data.content.toString());
-                console.log('Received data for web search:', {id, summary, accessToken});
+async function websearchWorker() {
+  try {
+    console.log(`websearch worker started`);
+    await channel.consume("websearchQueue", async (data) => {
+      if (data !== null) {
+        const { id, summary, accessToken } = JSON.parse(
+          data.content.toString()
+        );
+        // console.log('Received data for web search:', {id, summary, accessToken});
 
-                const webSearchResults = await webSearch(summary, accessToken);
+        const webSearchResults = await webSearch(summary, accessToken);
 
-                if (!webSearchResults) {
-                    console.error('No web search results found for ID:', id);
-                    channel.ack(data);
-                    return;
-                }
-                
-                await publisher.publish(CHANNEL, JSON.stringify({
-                    task_id: id,
-                    result: webSearchResults,
-                }));
-                // console.log(`Web search results for ID ${id}:`, webSearchResults);
+        if (!webSearchResults) {
+          console.error("No web search results found for ID:", id);
+          channel.ack(data);
+          return;
+        }
 
-                // Acknowledge the message
-                channel.ack(data);
-            }
-        });
-    } catch (error) {
-        console.error('Error in web search worker:', error);
+        await publisher.publish(
+          CHANNEL,
+          JSON.stringify({
+            task_id: id,
+            result: webSearchResults,
+          })
+        );
+        console.log("Data published from websearch worker");
+        // console.log(`Web search results for ID ${id}:`, webSearchResults);
+
+        // Acknowledge the message
         channel.ack(data);
-    }
+      }
+    });
+  } catch (error) {
+    console.error("Error in web search worker:", error);
+    channel.ack(data);
+  }
 }
-
 
 module.exports = {
-    connectQueue,
-    sendDataToSummaryQueue,
-    sendDataToWebSearchQueue,
-    summaryWorker,
-    websearchWorker
+  connectQueue,
+  sendDataToSummaryQueue,
+  sendDataToWebSearchQueue,
+  summaryWorker,
+  websearchWorker,
 };
-
-
--- Server/controllers/Auth.js
+++ Server/controllers/Auth.js
@@ -1,49 +1,48 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 exports.login = async (req, res) => {
   try {
-    console.log(req.headers);
-    const accessToken = req.headers['authorization']?.replace('Bearer ', '');
-    console.log(accessToken);
+    // console.log(req.headers);
+    const accessToken = req.headers["authorization"]?.replace("Bearer ", "");
+    // console.log(accessToken);
     const { id, email } = req.body;
 
-    if(!accessToken) {
+    if (!accessToken) {
       return res.status(400).json({
         success: false,
-        message: 'Access token is required',
+        message: "Access token is required",
       });
     }
 
     const payload = {
-        accessToken: accessToken,
-        id: id,
-        email: email,
-    }
+      accessToken: accessToken,
+      id: id,
+      email: email,
+    };
 
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: '12h', // Token expiration time
+      expiresIn: "12h", // Token expiration time
     });
 
     const cookieOptions = {
-        expires: new Date(Date.now() + 12 * 60 * 60 * 1000), // 12 hours
-        httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
-    }
+      expires: new Date(Date.now() + 12 * 60 * 60 * 1000), // 12 hours
+      httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+    };
 
-    res.cookie('token', token, cookieOptions);
+    res.cookie("token", token, cookieOptions);
 
     return res.status(200).json({
       success: true,
-      message: 'Login successful',
+      message: "Login successful",
     });
-
   } catch (error) {
-    console.error('Login error:', error);
+    console.error("Login error:", error);
     return res.status(500).json({
       success: false,
-      message: 'Login failed',
+      message: "Login failed",
       error: error.message,
     });
   }
-}
+};
